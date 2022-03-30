bbec955d265af8b3b6ebdaef68288907f15eeede
Calvin Li <ctl2124@columbia.edu>
2022-03-29 21:11:51 -0400

    beginnings of card selection

--- Assets/Scripts/UpdateSprite.cs
+++ Assets/Scripts/UpdateSprite.cs
@@ -9,12 +9,14 @@ public class UpdateSprite : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Selectable selectable;
     private Solitaire solitaire;
+    private UserInput userInput;
 
     // Start is called before the first frame update
     void Start()
     {
         List<string> deck = Solitaire.GenerateDeck();
         solitaire = FindObjectOfType<Solitaire>();
+        userInput = FindObjectOfType<UserInput>();
 
         int i = 0;
         foreach (string card in deck)
@@ -41,6 +43,16 @@ public class UpdateSprite : MonoBehaviour
         {
             spriteRenderer.sprite = cardBack;
         }
-        
+        if (userInput.slot1)
+        {
+            if (name == userInput.slot1.name)
+            {
+                spriteRenderer.color = Color.yellow;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
+        }
     }
 }
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+    public GameObject slot1;
     private Solitaire solitaire;
     // Start is called before the first frame update
     void Start()
     {
         solitaire = FindObjectOfType<Solitaire>();     
+        slot1 = this.gameObject;
     }
 
     // Update is called once per frame
@@ -34,7 +36,7 @@ public class UserInput : MonoBehaviour
                 else if (hit.collider.CompareTag("Card"))
                 {
                     // clicked card
-                    Card();
+                    Card(hit.collider.gameObject);
                 }
                 else if (hit.collider.CompareTag("Top"))
                 {
@@ -57,9 +59,34 @@ public class UserInput : MonoBehaviour
         solitaire.DealFromDeck();
     }
 
-    void Card()
+    void Card(GameObject selected)
     {
         print("Clicked on Card");
+
+        // if the card clicked on is facedown
+            // if the card clicked on is not blocked
+                //flip it over
+
+        // if the card clicked on is in the deck pile with the trips
+            // if it is not blocked
+                // select it      
+
+        // if the card is face up
+            // if there is no card currently selected
+                // select it
+
+        if (slot1 == this.gameObject)
+        {
+            slot1 = selected;            
+        }
+            // if there is already a card selected (and it is not the same card)
+                // if the new card is eligible to stack on the old card
+                    // stack it
+                // else
+                    // select the new card
+            // else if there is already a card slected and it is the same card
+                // if the time is short enough then it is a double click
+                    // if the card is eligible to fly to top then do it
     }
 
     void Top()
